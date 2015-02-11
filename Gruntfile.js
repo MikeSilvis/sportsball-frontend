20b4a64b803ccd5c2631f4c4dbe683cfe9f8681e
Mike Silvis <mikesilvis@gmail.com>
2015-02-11 09:20:21 -0800

    trying to remove the bs

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,24 +61,6 @@ module.exports = function (grunt) {
       }
     },
 
-    copy: {
-      dist: {
-          files: [{
-              expand: true,
-              dest: '<%= yeoman.dist %>',
-              cwd: 'heroku',
-              src: '*',
-              rename: function (dest, src) {
-                  var path = require('path');
-                  if (src === 'distpackage.json') {
-                      return path.join(dest, 'package.json');
-                  }
-                  return path.join(dest, src);
-              }
-          }]
-      }
-    }
-
     // The actual grunt server settings
     connect: {
       options: {
